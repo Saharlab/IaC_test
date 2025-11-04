ba2596e4bb41dc057822acb2428418a0f5f9e227
Saharlab <sahar.doukali@wavestone.com>
2025-11-04 15:55:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,3 +28,46 @@ resource "azurerm_storage_account" "sg2" {
   account_replication_type = "LRS"
   allow_nested_items_to_be_public = true
 
+resource "azurerm_mysql_flexible_server" "serverformation1" { 
+  name = "serverformationiac"
+location = var.location
+resource_group_name = var.rg_name
+administrator_login = "Saharlab"
+administrator_password = "Azerty0000@"
+  sku_name = "B_Standard_B1ms"
+  version = "8.0.21"
+  geo_redundant_backup_enabled = false
+  storage { 
+    auto_grow_enabled = false
+    size_gb = 20
+    io_scaling_enabled = false
+    iops = 360
+} 
+} 
+resource "azurerm_mysql_flexible_server_configuration" "ssl_config" {
+  name = "require_secure_transport"
+  resource_group_name = var.rg_name
+  server_name = azurerm_mysql_flexible_server.serverformation1.name
+  value = "OFF"
+}
+
+resource "azurerm_mysql_flexible_database" "mysqldb1" { 
+  name = "mysqldb1-iac"
+  resource_group_name = var.rg_name
+  server_name = azurerm_mysql_flexible_server.serverformation1.name
+  charset = "utf8"
+  collation = "utf8_unicode_ci"
+    depends_on = [ azurerm_mysql_flexible_server.serverformation1 ] 
+} 
+
+#Configure firewall to open access 
+resource "azurerm_mysql_flexible_server_firewall_rule" "mysqlfwrule1" { 
+  name = "mysqlfwrule1-iac"
+  resource_group_name = var.rg_name
+  server_name = azurerm_mysql_flexible_server.serverformation1.name
+    start_ip_address = "0.0.0.0"
+    end_ip_address = "255.255.255.255"
+    depends_on = [ azurerm_mysql_flexible_server.serverformation1, 
+    azurerm_mysql_flexible_database.mysqldb1 ] 
+}
+
